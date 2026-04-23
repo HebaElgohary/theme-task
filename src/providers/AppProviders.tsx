e97ffad53b405a98b6a9b70dd133049f77945138
HebaElgohary <hebasamir535@gmail.com>
2026-04-23 23:33:53 +0200

    feat: add nextintelclient provider

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,9 +1,16 @@
-import React, { ReactNode } from 'react'
+import  { ReactNode } from 'react'
+import {NextIntlClientProvider} from 'next-intl'
 import ThemesProvider from './ThemeProvider'
 export default function AppProviders({children}:{children:ReactNode}) {
   return (
     <ThemesProvider>
+
+        <NextIntlClientProvider >
+
         {children}
+        </NextIntlClientProvider>
     </ThemesProvider>
+
+
   )
 }
